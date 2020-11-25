a41a3ae5141e839b5d88430e23126bc553ec60ab
LubluLabi <71341849+LubluLabi@users.noreply.github.com>
2020-11-25 12:43:44 +0300

    Add files via upload

---
+++ 7 лаба.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <windows.h>
+#include <clocale>
+#pragma warning(disable : 4996) - scanf
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	char* s = NULL;
+	int i, n;
+	printf("Размер: ");
+	scanf("%d", &n);
+	s = (char*)calloc(n, sizeof(char));
+	printf("Вводите текст: ");
+	rewind(stdin);
+	gets_s(s, n);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '.')
+		{
+			s[i + 2] = toupper(s[i + 2]);
+		}
+		printf("%c", s[i]);
+	}
+	getchar();
+	return 0;
+}
